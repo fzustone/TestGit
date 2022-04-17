92090ac857314425220a01a68ed4b029ff1d143a
chenly <cly3322@gmail.com>
2022-04-17 14:34:34 +0800

    feature-02-test02

---
+++ src/main/java/com/example/testgit/UserController.java
@@ -0,0 +1,12 @@
+package com.example.testgit;
+
+/**
+ * @author chenly
+ * @create 2022-04-17 14:33
+ */
+public class UserController {
+
+    private String getName() {
+        return "chenly";
+    }
+}
